41bdab27f3a41e7bdebcdb005d3f38eae4d31102
unknown <89234857879@mail.ru>
2019-12-13 12:16:08 +0700

    Updates

---
+++ precache-manifest.2488d66bc486f1f19abcb76b5c726f4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2344988f55119a5f74d0659cfe68e42c",
+    "url": "/my-react-game/index.html"
+  },
+  {
+    "revision": "4c60623fafb35e20af2c",
+    "url": "/my-react-game/static/css/main.fcccad85.chunk.css"
+  },
+  {
+    "revision": "e8d781d67a10a0bc229a",
+    "url": "/my-react-game/static/js/2.d884f893.chunk.js"
+  },
+  {
+    "revision": "4c60623fafb35e20af2c",
+    "url": "/my-react-game/static/js/main.1a937262.chunk.js"
+  },
+  {
+    "revision": "961b426c52dc6ac061eb",
+    "url": "/my-react-game/static/js/runtime-main.8a114ab7.js"
+  }
+]);
